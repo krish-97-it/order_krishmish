20770c3f36a62eedf74f79a577c94a93ed4a523e
KRISHNENDU NANDY <krishnendunandy6@gmail.com>
2024-09-14 19:45:41 +0530

    branch-34 #comment my cart api creation and save data to db

--- frontend/src/pages/user-order-history.js
+++ frontend/src/pages/user-order-history.js
@@ -1,11 +1,22 @@
 import React from "react";
 import OrderHistory from "../components/order-history";
+import sadSvg from '../assets/sad-face-in-rounded-square-svgrepo-com.svg';
 
 const OrderHistoryPage = () => {
     return(
+        // <div className="app-body">
+        //     <div className="main-content mb-3" style={{marginTop:"61px"}}>
+        //         <div className="container">
+        //             <OrderHistory/>
+        //         </div>
+        //     </div>
+        // </div>
         <div className="app-body">
-            <div className="main-content mb-3" style={{marginTop:"61px"}}>
-                <div className="container">
+            <div className="main-content container">
+                <div className="error-page-msg-section">
+                    <div className="sad-face-icon" style={{backgroundColor:"white", paddingTop:"10px"}}>
+                        <img src={sadSvg} alt="error"/>
+                    </div>
                     <OrderHistory/>
                 </div>
             </div>
