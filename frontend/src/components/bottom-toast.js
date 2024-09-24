9d4ab9bd264b5fd4cb61f6d3cdb336980c3197df
Krishnendu Nandy <krishnendunandy6@gmail.com>
2024-09-25 01:27:14 +0530

    branch-39 #comment lastorderd item on cart page, apply offer function fix2

--- frontend/src/components/bottom-toast.js
+++ frontend/src/components/bottom-toast.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 export default function BottomToast(){
 
-
     const [hideContainer, updateHideContainer] = useState('false')
     function hideToastContainer(){
         updateHideContainer('true');
